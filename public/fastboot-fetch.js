f7c451feb7bf4fd855917c6d7fc48bc782ae74a0
Jan Bobisud <me@bobisjan.com>
2018-10-27 16:22:05 +0200

    [WIP] Require fix

--- public/fastboot-fetch.js
+++ public/fastboot-fetch.js
@@ -1,7 +1,7 @@
 /* globals define FastBoot */
 (function() {
   define('fetch', ['exports'], function(self) {
-    var AbortControllerPolyfill = FastBoot.require('abortcontroller-polyfill/dist/cjs-ponyfill');
+    var AbortControllerPolyfill = FastBoot.require('abortcontroller-polyfill/dist/cjs-ponyfill.js');
     var nodeFetch = FastBoot.require('node-fetch');
     var abortableFetch = AbortControllerPolyfill.abortableFetch({
       fetch: nodeFetch,
@@ -11,7 +11,7 @@
     self['default'] = abortableFetch.fetch;
     self['Request'] = abortableFetch.Request;
 
-    self['Headers'] = nodeFetch.Headers;    
+    self['Headers'] = nodeFetch.Headers;
     self['Response'] = nodeFetch.Response;
 
     self['AbortController'] = AbortControllerPolyfill.AbortController;
